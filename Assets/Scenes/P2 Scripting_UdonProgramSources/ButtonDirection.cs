654a69e945f4665a260e348790dee99883cfc169
yoominel <yoon.g@northeastern.edu>
2026-02-20 14:16:32 -0500

    Prototype 3 Match Puzzle Scripts
    Making match puzzle scripts within Prototype 3 Puzzle Scripts scene. Created 3 prefabs for use in full game assembly: SlideBoxPrefab, SilderGoalZonePrefab, and KeyLocationConditionCheckTest.

---
+++ Assets/Scenes/P2 Scripting_UdonProgramSources/ButtonDirection.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ButtonDirection : UdonSharpBehaviour
+{
+    public ObjectAxisMover objectMovedByButton; // This object should have ObjectAxisMover script attached
+    public Vector3 moveDirection; // Direction of movement this button makes, assign in Inspector (e.g. 1, 0, 0)
+
+    public override void Interact()
+    {
+        if (objectMovedByButton != null)
+        {
+            objectMovedByButton.MoveObjectByUnit(moveDirection); // Call MoveObjectByUnit function with assigned direction input
+        }
+    }
+}
