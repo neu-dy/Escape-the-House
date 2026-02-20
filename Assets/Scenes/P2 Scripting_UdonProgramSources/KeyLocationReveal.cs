0d7ba3b9d59350c3f0feb303f183f3673c51475e
yoominel <yoon.g@northeastern.edu>
2026-02-20 17:56:30 -0500

    Prototype 3 script full merge
    Finished compiling all scripts together in MAINSCENE for final prototype.

--- Assets/Scenes/P2 Scripting_UdonProgramSources/KeyLocationReveal.cs
+++ Assets/Scenes/P2 Scripting_UdonProgramSources/KeyLocationReveal.cs
@@ -8,8 +8,8 @@ public class KeyLocationReveal : UdonSharpBehaviour
 {
     private int successCount = 0;
     [SerializeField] private int numberOfPuzzleBoxes = 3; // Puzzle slider boxes needed for reward
-    // [SerializeField] private LightManager lightClue;
-    public GameObject cubeTest;
+    public Light_Controller lightController;
+    [SerializeField] private int lightRevealNumber; // This is the set of key location that gets revealed
 
     public void AddNumberOfBoxes()
     {
@@ -21,7 +21,7 @@ public class KeyLocationReveal : UdonSharpBehaviour
     {
         if (successCount >= numberOfPuzzleBoxes)
         {
-            cubeTest.SetActive(true);
+            lightController.TurnOnStage(lightRevealNumber);
         }
     }
 }
